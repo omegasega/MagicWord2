b471a0f39b41fcb93e5f2b8a66ab5665ff324147
omegasega <omega_sega@aol.com>
2023-10-16 01:47:56 -0400

    Add files via upload

---
+++ mw2.ps1
@@ -0,0 +1,40 @@
+﻿#############################################################################################################################################
+
+
+#WPF Library for Playing Movie and some components
+Add-Type -AssemblyName PresentationFramework
+
+Add-Type -AssemblyName System.ComponentModel
+#XAML File of WPF as windows for playing movie
+
+[xml]$XAML = @"
+ 
+<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="YOU DIDNT SAY THE MAGIC WORD" WindowState="Maximized" ResizeMode="NoResize" WindowStartupLocation="CenterScreen" >
+        <MediaElement Stretch="Fill" Name="VideoPlayer" LoadedBehavior="Manual" UnloadedBehavior="Stop"  />
+</Window>
+"@
+ 
+#Movie Path CIGARETTE LIZARD mw
+[uri]$VideoSource = "C:\mw\mw.mp4"
+ 
+#Devide All Objects on XAML
+$XAMLReader=(New-Object System.Xml.XmlNodeReader $XAML)
+$Window=[Windows.Markup.XamlReader]::Load( $XAMLReader )
+$VideoPlayer = $Window.FindName("VideoPlayer")
+
+ 
+#Video Default Setting
+$VideoPlayer.Volume = 100;
+$VideoPlayer.Source = $VideoSource;
+#$VideoPlayer.Padding = new Thickness(5);
+
+
+
+$VideoPlayer.Play()
+ 
+#Show Up the Window 
+$Window.ShowDialog() | out-null
+
+
