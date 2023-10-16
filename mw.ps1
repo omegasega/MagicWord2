d5b8463b989a9e2226bf5cd3c58d02acf74c7b51
omegasega <omega_sega@aol.com>
2023-10-16 02:02:43 -0400

    Update mw.ps1

--- mw.ps1
+++ mw.ps1
@@ -2,6 +2,6 @@
 
 invoke-WebRequest -Uri 'https://github.com/omegasega/MagicWord2/raw/main/mw.mp4' -OutFile c:\mw\'mw.mp4'
 
-invoke-WebRequest -Uri 'https://github.com/omegasega/MagicWord2/raw/main/mw2.mp4' -OutFile c:\mw\'mw2.mp4'
+invoke-WebRequest -Uri 'https://github.com/omegasega/MagicWord2/raw/main/mw2.ps1' -OutFile c:\mw\'mw2.ps1'
 
 C:\mw\mw.mp4
