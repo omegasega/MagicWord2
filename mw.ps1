acdbfc168d2609d7c7d980188874c6e5b55bc06d
omegasega <omega_sega@aol.com>
2023-10-16 01:49:45 -0400

    Update mw.ps1

--- mw.ps1
+++ mw.ps1
@@ -1,5 +1,5 @@
-#CIGARETTE LIZARD
+se#CIGARETTE LIZARD
 
-invoke-WebRequest -Uri 'https://github.com/CigaretteLizard/MagicWord/raw/main/mw.mp4' -OutFile c:\mw\'mw.mp4'
+invoke-WebRequest -Uri 'https://github.com/omegasega/MagicWord2/raw/main/mw.mp4' -OutFile c:\mw\'mw.mp4'
 
 C:\mw\mw.mp4
